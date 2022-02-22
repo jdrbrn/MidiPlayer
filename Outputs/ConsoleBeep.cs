f78d49d02c33fa7da9e3a242b6958709270563a8
jdrbrn <jdrbrn@users.noreply.github.com>
2022-02-21 21:18:36 -0500

    Add Console.Beep output method
    Added the ability to playback through Console.Beep calls. This method is only available on Windows.

---
+++ Outputs/ConsoleBeep.cs
@@ -0,0 +1,49 @@
+﻿using System.Timers;
+
+namespace MidiPlayer.Outputs
+{
+    internal class ConsoleBeep : IOutput
+    {
+
+        private Dictionary<int, int> _frequencies = new Dictionary<int, int>();
+
+        public void Output(ParsedTrack track)
+        {
+            // Check if running on Windows
+            if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows)) throw new Exception("Error: Can only use system beep output on Windows");
+            Console.WriteLine("Playing song...");
+            // Keep track of time
+            int time = 0;
+            // Iternate over notes
+            for (int i = 0; i<track.Notes.Count; i++)
+            {
+                // Convert timestamp and length to int for ease of use
+                int timestamp = (int)track.Notes[i].TimeStamp;
+                int length = (int)track.Notes[i].Length;
+                // Sleep until we hit timestamp
+                Thread.Sleep(timestamp - time);
+                // Update time to be at timestamp
+                time = timestamp;
+                // Play note if valid
+                if (track.Notes[i].Length > 0) Console.Beep(GetFrequency(track.Notes[i].Note), length);
+                // Update time to be after playing note
+                time += length;
+            }
+            Console.WriteLine("Done");
+        }
+
+        // Return frequency in Hz for a given note by Midi number
+        private int GetFrequency(int note)
+        {
+            // Check if frequency already calculated; If not calculate + store
+            if (!_frequencies.ContainsKey(note))
+            {
+                // Algorithm from https://newt.phys.unsw.edu.au/jw/notes.html
+                // f  =  2^((m−69)/12) * (440 Hz) where m = midi note number
+                _frequencies[note] = (int)(Math.Pow(2, (note - 69) / 12f) * 440);
+
+            }
+            return _frequencies[note];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine("Reading File");
 byte[] inputData = System.IO.File.ReadAllBytes(inputFile);
 Console.WriteLine("File Read");
 Console.WriteLine("Initializing IO");
-MidiPlayer.Outputs.IOutput outputModule = new MidiPlayer.Outputs.MidiFile();
+MidiPlayer.Outputs.IOutput outputModule = new MidiPlayer.Outputs.ConsoleBeep();
 MidiPlayer.MidiParser midiParser = new();
 MidiPlayer.ParsedTrack output = midiParser.ParseData(inputData);
 outputModule.Output(output);
