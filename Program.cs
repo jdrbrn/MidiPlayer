e975177ee7387edf7ad4f66d591bc2f6a86a414b
jdrbrn <jdrbrn@users.noreply.github.com>
2022-02-26 14:38:33 -0500

    Output set by runtime argument
    The output module is now set by a runtime argument instead of being hardcoded

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
 ﻿string inputFile;
+MidiPlayer.Output.IOutput outputModule;
 
-if (args.Count() > 0) inputFile = args[0];
+Dictionary<string, Func<MidiPlayer.Output.IOutput>> outputs = new Dictionary<string, Func<MidiPlayer.Output.IOutput>>
+{
+    ["MidiFile"] = new Func<MidiPlayer.Output.IOutput>(() => new MidiPlayer.Output.MidiFile("output.mid")),
+    ["ConsoleBeep"] = new Func<MidiPlayer.Output.IOutput>(() => new MidiPlayer.Output.ConsoleBeep()),
+    ["PassiveBuzzer"] = new Func<MidiPlayer.Output.IOutput>(() => new MidiPlayer.Output.PassiveBuzzer(18))
+};
+
+if (args.Count() < 2)
+{
+    Console.WriteLine("Usage: MidiPlayer.dll [output] [path to midi]");
+    return;
+}
+else if (!outputs.ContainsKey(args[0]))
+{
+    Console.WriteLine("Invalid output");
+    Console.Write("Options are: ");
+    foreach (string option in outputs.Keys) Console.Write(option + " ");
+    Console.WriteLine();
+    return;
+}
 else
 {
-    Console.WriteLine("Run with argument to midi file");
-    return;
+    inputFile = args[1];
+    outputModule = outputs[args[0]]();
 }
 
 Console.WriteLine("Opening file input.mid");
@@ -18,7 +38,6 @@ Console.WriteLine("Reading File");
 byte[] inputData = System.IO.File.ReadAllBytes(inputFile);
 Console.WriteLine("File Read");
 Console.WriteLine("Initializing IO");
-MidiPlayer.Output.IOutput outputModule = new MidiPlayer.Output.BuzzerTest(18);
 MidiPlayer.MidiParser midiParser = new();
 MidiPlayer.ParsedTrack output = midiParser.ParseData(inputData);
 outputModule.Output(output);
