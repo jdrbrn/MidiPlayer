a91e9ac1e93ab721b5d82772b189b46d5aeb52f4
jdrbrn <jdrbrn@users.noreply.github.com>
2022-02-26 01:27:20 -0500

    Add PassiveBuzzer output
    Supports passive buzzer devices via PWM GPIO

---
+++ Outputs/PassiveBuzzer.cs
@@ -0,0 +1,24 @@
+﻿using System.Device.Pwm.Drivers;
+// Requires libgpiod-dev package
+
+namespace MidiPlayer.Outputs
+{
+    internal class PassiveBuzzer : ToneGenerator
+    {
+
+        SoftwarePwmChannel softwarePwmChannel;
+
+        public PassiveBuzzer(int pin) : base()
+        {
+            softwarePwmChannel = new SoftwarePwmChannel(pin);
+        }
+
+        protected override void PlayNote(Note note)
+        {
+            softwarePwmChannel.Frequency = GetFrequency(note.NoteNum);
+            softwarePwmChannel.Start();
+            Thread.Sleep((int)note.Length);
+            softwarePwmChannel.Stop();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ Console.WriteLine("Reading File");
 byte[] inputData = System.IO.File.ReadAllBytes(inputFile);
 Console.WriteLine("File Read");
 Console.WriteLine("Initializing IO");
-MidiPlayer.Outputs.IOutput outputModule = new MidiPlayer.Outputs.ConsoleBeep();
+MidiPlayer.Outputs.IOutput outputModule = new MidiPlayer.Outputs.PassiveBuzzer(18);
 MidiPlayer.MidiParser midiParser = new();
 MidiPlayer.ParsedTrack output = midiParser.ParseData(inputData);
 outputModule.Output(output);
