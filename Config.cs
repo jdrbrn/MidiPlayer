c0ffca254c6a387e82f2f015f6e391b4937cca4a
jdrbrn <jdrbrn@users.noreply.github.com>
2022-03-04 17:34:44 -0500

    Config files used to init output modules
    Output modules are now loaded based off data stored in a config file instead of a command line argument. This allows for more detailed output module configuration (ex. pin numbers, file name etc...).

---
+++ Config.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+namespace MidiPlayer
+{
+    internal class Config
+    {
+        public OutputChannel[] OutputChannels { get; set; }
+
+        public Config(OutputChannel[] outputChannels) 
+        {
+            OutputChannels = outputChannels;
+        }
+
+        // Returns a default config based around a single channel MidiFile output
+        public static Config DefaultConfig()
+        {
+            return new Config
+            (
+                new OutputChannel[]
+                {
+                    new OutputChannel()
+                    {
+                        OutputModule = "MidiFile",
+                        Args = new string[] { "output.mid" }
+                    }
+                }
+            );
+        }
+
+        public static void SaveConfig(Config config, string file)
+        {
+            string jsonString = JsonSerializer.Serialize(config);
+            File.WriteAllText(file, jsonString);
+        }
+
+        public static Config LoadConfig(string file)
+        {
+            if (!File.Exists(file)) throw new FileNotFoundException(file);
+
+            string jsonString = File.ReadAllText(file);
+            return JsonSerializer.Deserialize<Config>(jsonString)!;
+        }
+
+        public struct OutputChannel
+        {
+            public string OutputModule { get; set; }
+            public string[] Args { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,43 +1,95 @@
 ﻿string inputFile;
-MidiPlayer.Output.IOutput outputModule;
+MidiPlayer.Config config;
 
-Dictionary<string, Func<MidiPlayer.Output.IOutput>> outputs = new Dictionary<string, Func<MidiPlayer.Output.IOutput>>
+// Dictionary acts as a converter from string to an Output Module
+// (Eventually move out to a different class)
+// Each delegate function also checks for the arguments needed for each Output Module
+Dictionary<string, Func<string[], MidiPlayer.Output.IOutput>> outputs = new()
 {
-    ["MidiFile"] = new Func<MidiPlayer.Output.IOutput>(() => new MidiPlayer.Output.MidiFile("output.mid")),
-    ["ConsoleBeep"] = new Func<MidiPlayer.Output.IOutput>(() => new MidiPlayer.Output.ConsoleBeep()),
-    ["PassiveBuzzer"] = new Func<MidiPlayer.Output.IOutput>(() => new MidiPlayer.Output.PassiveBuzzer(18)),
+    // Takes one argument as the name of the output file
+    ["MidiFile"] = new Func<string[], MidiPlayer.Output.IOutput>((string[] args) => {
+        string outputFile;
+        if (args.Length > 1) Console.WriteLine("Warning: Extraneous arguments passed to output module MidiFile, ignoring...");
+        if (args.Length > 0) outputFile = args[0];
+        else
+        {
+            Console.WriteLine("Warning: No argument passed to output module MidiFile, using default output file(output.mid)");
+            outputFile = "output.mid";
+        }
+        return new MidiPlayer.Output.MidiFile(outputFile);
+    }),
+    // Takes 0 arguments
+    // Also check if on Windows as ConsoleBeep isn't supported on any other platforms
+    ["ConsoleBeep"] = new Func<string[], MidiPlayer.Output.IOutput>((string[] args) => {
+        if (args.Length > 0) Console.WriteLine("Warning: Extraneous arguments passed to output module ConsoleBeep, ignoring...");
+        // Check if running on Windows
+        if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows)) throw new Exception("Error: Can only use ConsoleBeep output module on Windows");
+        return new MidiPlayer.Output.ConsoleBeep();
+    }),
+    // Needs one argmuent for the GPIO Pin number
+    ["PassiveBuzzer"] = new Func<string[], MidiPlayer.Output.IOutput>((string[] args) => {
+        int pin;
+        if (args.Length > 1) Console.WriteLine("Warning: Extraneous arguments passed to output module PassiveBuzzer, ignoring...");
+        if (args.Length > 0)
+        {
+            if (!Int32.TryParse(args[0], out pin)) throw new Exception("Error: invalid arugment \"" + args[0] + "\" for output module PassiveBuzzer");
+        }
+        else
+        {
+            throw new Exception("Error: No pin number passed for PassiveBuzzer output module");
+        }
+        return new MidiPlayer.Output.PassiveBuzzer(pin);
+    }),
 };
 
-if (args.Count() < 2)
+// If either too few or too many arguments print help test
+if (args.Length < 1 || args.Length > 2)
 {
-    Console.WriteLine("Usage: MidiPlayer.dll [output] [path to midi]");
+    Console.WriteLine("Usage: MidiPlayer.dll midi_path [config_path]");
     return;
-}
-else if (!outputs.ContainsKey(args[0]))
-{
-    Console.WriteLine("Invalid output");
-    Console.Write("Options are: ");
-    foreach (string option in outputs.Keys) Console.Write(option + " ");
-    Console.WriteLine();
-    return;
-}
-else
-{
-    inputFile = args[1];
-    outputModule = outputs[args[0]]();
 }
 
-Console.WriteLine("Opening file input.mid");
-if (!File.Exists(inputFile))
+// Check if Midi input file exists
+if (!File.Exists(args[0]))
 {
-    Console.WriteLine("File " + inputFile + " not found");
+    Console.WriteLine("Midi File " + args[0] + " not found");
     return;
 }
-Console.WriteLine("File Found");
-Console.WriteLine("Reading File");
+else inputFile = args[0];
+
+// Check if config file passed via argument
+if (args.Length==2)
+{
+    // Check if config file exists
+    if (!File.Exists(args[1]))
+    {
+        Console.WriteLine("Config File " + args[0] + " not found");
+        return;
+    }
+    else config = MidiPlayer.Config.LoadConfig(args[1]);
+}
+// If no config file argument check for a config.json in same directory
+else
+{
+    // If config file doesn't exist then output some help text
+    if (!File.Exists("config.json"))
+    {
+        Console.WriteLine("No config file passed in and config file not found!");
+        Console.WriteLine("Creating config.json with default values.");
+        Console.WriteLine("Please edit and restart program");
+        MidiPlayer.Config.SaveConfig(MidiPlayer.Config.DefaultConfig(), "config.json");
+        return;
+    }
+    else config = MidiPlayer.Config.LoadConfig("config.json");
+}
+
+// Create output module from the data found in the config file
+MidiPlayer.Output.IOutput outputModule = outputs[config.OutputChannels[0].OutputModule](config.OutputChannels[0].Args);
+
+Console.WriteLine("Opening file " + inputFile);
 byte[] inputData = System.IO.File.ReadAllBytes(inputFile);
-Console.WriteLine("File Read");
-Console.WriteLine("Initializing IO");
+Console.WriteLine("Parsing file");
 MidiPlayer.MidiParser midiParser = new();
 MidiPlayer.ParsedTrack output = midiParser.ParseData(inputData);
+Console.WriteLine("Outputting data");
 outputModule.Output(output);
