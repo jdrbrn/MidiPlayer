8844088cb648e2a1a0f0c971960318e1525d1548
jdrbrn <jdrbrn@users.noreply.github.com>
2022-03-04 17:36:01 -0500

    Remove extra platform check for ConsoleBeep
    Check is now done in the function that creates the ConsoleBeep output

--- Output/ConsoleBeep.cs
+++ Output/ConsoleBeep.cs
@@ -2,12 +2,6 @@
 {
     internal class ConsoleBeep : ToneGenerator
     {
-        public ConsoleBeep() : base()
-        {
-            // Check if running on Windows
-            if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows)) throw new Exception("Error: Can only use system beep output on Windows");
-        }
-
         protected override void PlayNote(MidiPlayer.Note note)
         {
             Console.Beep(GetFrequency(note.NoteNum), Convert.ToInt32(note.Length));
